aa8f66c18c3e0d7f2d4bb07fe09a0d703522c5db
Andreas Tielmann <a.tielmann@deloitte.de>
2021-03-06 10:39:36 +0100

    Fix Bug for one Column

--- Sources/SwiftTableView/DefaultTableStyle.swift
+++ Sources/SwiftTableView/DefaultTableStyle.swift
@@ -12,7 +12,7 @@ public struct DefaultTableStyle<Header: View, Cell: View>: TableStyle {
     public func makeBody(configuration: TableStyleConfiguration<Header, Cell>) -> some View {
         VStack {
             LazyVGrid(columns: configuration.gridItems) {
-                ForEach(0..<configuration.columns, id: \.self) { column in
+                ForEach(0..<configuration.columns) { column in
                     configuration.header(column: column)
                 }
             }
@@ -20,9 +20,10 @@ public struct DefaultTableStyle<Header: View, Cell: View>: TableStyle {
             Color(UIColor.label).frame(height: 1)
             ScrollView {
                 LazyVGrid(columns: configuration.gridItems, spacing: 8) {
-                    ForEach(0..<configuration.rows, id: \.self) { row in
-                        ForEach(0..<configuration.columns, id: \.self) { column in
+                    ForEach(0..<configuration.rows) { row in
+                        ForEach(0..<configuration.columns) { column in
                             configuration.cell(column: column, row: row)
+                                .id("\(column)-\(row)")
                         }
                     }
                 }
